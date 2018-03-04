c4ef15fc414500f911d395da70c1e80f980652eb
Alison Walter <awalter2@wisc.edu>
2018-03-04 16:50:52 +0100

    Adapt logo script to new imglib2-roi API
    See http://forum.imagej.net/t/vote-now-for-the-new-imagej-logo/8895/10.
    
    Signed-off-by: Jan Eglinger <jan.eglinger@gmail.com>

--- ImageJ_Logo.groovy
+++ ImageJ_Logo.groovy
@@ -1,24 +1,29 @@
-/* Create logo mask */
-
+#@OUTPUT img
 import net.imglib2.roi.Masks
 import net.imglib2.roi.geom.GeomMasks
+import bdv.util.Bdv
+import bdv.util.BdvFunctions
+import net.imglib2.FinalInterval
+import net.imglib2.view.Views
+import net.imglib2.FinalInterval
 
-I = GeomMasks.closedBox([30.5, 10.5] as double[], [40.5, 60.5] as double[])
 
-Jbar      = GeomMasks.closedBox    ([45.5, 30.5] as double[], [70.5, 40.5] as double[])
-Jstem     = GeomMasks.closedBox    ([60.5, 30.5] as double[], [70.5, 70.5] as double[])
-Jouter    = GeomMasks.OpenSphere   ([50.5, 70.5] as double[], 20.0)
-Jinner    = GeomMasks.closedSphere ([50.5, 70.5] as double[], 10.0)
-Jclipping = GeomMasks.openBox      ([ 0.5, 70.5] as double[], [100.5, 100.5] as double[])
+I = GeomMasks.closedWritableBox([30.5, 10.5] as double[], [40.5, 60.5] as double[])
+
+Jbar      = GeomMasks.closedWritableBox([45.5, 30.5] as double[], [70.5, 40.5] as double[])
+Jstem     = GeomMasks.closedWritableBox([60.5, 30.5] as double[], [70.5, 70.5] as double[])
+Jouter    = GeomMasks.openWritableSphere([50.5, 70.5] as double[], 20.0)
+Jinner    = GeomMasks.closedWritableSphere([50.5, 70.5] as double[], 10.0)
+Jclipping = GeomMasks.openWritableBox([ 0.5, 70.5] as double[], [100.5, 100.5] as double[])
 
 J = Jouter.minus(Jinner).and(Jclipping).or(Jbar).or(Jstem)
 
-slide  = GeomMasks.closedBox([25.5, 66.5] as double[], [45.5, 68.5] as double[])
+slide  = GeomMasks.closedWritableBox([25.5, 66.5] as double[], [45.5, 68.5] as double[])
 
 /* Create frame */
 
-frame1 = GeomMasks.closedBox([ 0.5,  0.5] as double[], [100.5, 100.5] as double[])
-frame2 = GeomMasks.closedBox([ 1.5,  1.5] as double[], [99.5, 99.5] as double[])
+frame1 = GeomMasks.closedWritableBox([ 0.5,  0.5] as double[], [100.5, 100.5] as double[])
+frame2 = GeomMasks.closedWritableBox([ 1.5,  1.5] as double[], [99.5, 99.5] as double[])
 frame  = frame1.minus(frame2)
 
 /* Combine mask */
@@ -26,10 +31,6 @@ frame  = frame1.minus(frame2)
 mask = Masks.toRealRandomAccessibleRealInterval(I.or(J).or(slide).or(frame))
 
 /* Show combined mask in BigDataViewer */
-
-import bdv.util.Bdv
-import bdv.util.BdvFunctions
-import net.imglib2.FinalInterval
 
 BdvFunctions.show(
 				mask,
@@ -40,10 +41,6 @@ BdvFunctions.show(
 				Bdv.options() )
 
 /* Use Imglib2 Views to raster the mask */
-
-#@OUTPUT img
-import net.imglib2.view.Views
-import net.imglib2.FinalInterval
 
 img = Views.interval(Views.raster(mask), new FinalInterval(
 						[mask.realMin( 0 ), mask.realMin( 1 ) ] as long[],
